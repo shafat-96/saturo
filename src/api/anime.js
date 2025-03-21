103e7584f92ac4ada8730baebddf44fc781fc28e
shafat-96 <animeexost61@gmail.com>
2025-03-21 14:13:58 +0600

    Update anime.js

--- src/api/anime.js
+++ src/api/anime.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-const BASE_URL = 'https://satoru-flame.vercel.app';
+const BASE_URL = 'https://hindi-api-three.vercel.app';
 
 // Helper function to capitalize words
 function capitalizeWords(str) {
